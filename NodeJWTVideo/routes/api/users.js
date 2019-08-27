680278de80e4bbfdb396afca179ca916bfba08ee
Leszek <leszek.janczewski@gmail.com>
2019-08-27 23:08:39 +0200

    dodanie obslugi bledow

--- NodeJWTVideo/routes/api/users.js
+++ NodeJWTVideo/routes/api/users.js
@@ -5,17 +5,27 @@ const router = Router();
 
 router.get('/', (req, res) => {
     User.find({}, function (err, users) {
+        if (err) {
+            return res.status(500).send({ err });
+        }
         return res.send(users);
     })
 });
 
 router.post('/', (req, res) => {
     const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).send({ err: 'Required Fields not found: username' });
+    }
     const newUser = new User({
         username: username
     });
     newUser.save(function (err, model) {
-        return res.send(model);
+        if (err) {
+            return res.status(400).send({ err });
+        }
+        return res.status(201).send(model);
     });
 });
 
