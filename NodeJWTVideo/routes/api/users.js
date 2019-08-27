bdd7d662c8d1e4e233c5b7f3badee8f7b30e4b76
Leszek <leszek.janczewski@gmail.com>
2019-08-27 23:28:09 +0200

    dodanie obslugi szyfrowanego hasla przy zkladaniu uzytkownikow

--- NodeJWTVideo/routes/api/users.js
+++ NodeJWTVideo/routes/api/users.js
@@ -12,14 +12,39 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
-    const { username } = req.body;
+router.post('/password', (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400)
+            .send({
+                err: `Required Fields not found: ${!username ? 'username' : ''} ${!password ? 'password' : ''}`
+            });
+    }
+    User.findOne({ username: username }, function (err, userModel) {
+        if (err) return res.status(400).send(err);
 
-    if (!username) {
-        return res.status(400).send({ err: 'Required Fields not found: username' });
+        if (!userModel) return res.status(400).send({ err: 'Cannot find user' });
+
+        return userModel.comparePassword(password, function (err, isMatch) {
+            if (err) return res.status(400).send(err);
+
+            return res.send({ correct: isMatch });
+        });
+    });
+});
+
+
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).send({
+            err: `Required Fields not found: ${!username ? 'username' : ''} ${!password ? 'password' : ''}`
+        });
     }
     const newUser = new User({
-        username: username
+        username: username,
+        password: password
     });
     newUser.save(function (err, model) {
         if (err) {
