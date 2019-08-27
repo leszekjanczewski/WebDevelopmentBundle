90c7d78fe4fef3581798620d673a8e22e8e747bc
Leszek <leszek.janczewski@gmail.com>
2019-08-27 22:40:39 +0200

    dodanie obsługi bazy danych z Atlas MongoDB

--- NodeJWTVideo/index.js
+++ NodeJWTVideo/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import parser from 'body-parser';
+import mongoose, { Schema } from 'mongoose';
+mongoose.connect('mongodb+srv://user:kWX02mGt3gvY@cluster0-zd33v.mongodb.net/test?retryWrites=true&w=majority')
+
+const userSchema = new Schema({
+    username: { type: String, required: true, index: { unique: true } }
+});
+
+const User = mongoose.model('user', userSchema);
 
 const app = new express();
-
 app.use(parser.urlencoded({
     extended: false
 }));
@@ -15,8 +22,20 @@ app.get('/world', (req, res) => {
     return res.send('World!');
 });
 
+app.get('/users', (req, res) => {
+    User.find({}, function (err, users) {
+        return res.send(users);
+    })
+});
+
 app.post('/', (req, res) => {
-    return res.send(req.body);
-})
+    const { username } = req.body;
+    const newUser = new User({
+        username: username
+    });
+    newUser.save(function (err, model) {
+        return res.send(model);
+    });
+});
 
 app.listen(process.env.PORT || 3000);
